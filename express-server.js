fd3e7e5edb55016ece9fc51a4672a317e4514d9e
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-21 22:37:22 +0000

    made a working urls page

--- express-server.js
+++ express-server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
 
 app.get('/', (req, res) => {
   res.send("Hello!");
@@ -14,8 +15,13 @@ app.get('/', (req, res) => {
 
 app.get('/hello', (req,res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
+});
+
+app.get('/urls', (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
-})
+});
