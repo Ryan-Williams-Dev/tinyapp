dd2ce45718f13c288654a9cefc29b9b0d7c04b02
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-22 23:08:51 +0000

    implemented log in and logour function using only username and cookies

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
@@ -11,16 +13,30 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
+
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+})
 
 app.get('/hello', (req,res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
+  const templateVars = {
+    urls: urlDatabase,
+    username: req.cookies['username']  
+  };
   res.render('urls_index', templateVars);
 });
 
@@ -35,6 +51,9 @@ app.post("/urls", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
+  const templateVars = {
+    username: req.cookies['username']
+  }
   res.render("urls_new");
 });
 
@@ -45,7 +64,11 @@ app.get('/u/:shortURL', (req, res) => {
 })
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies['username']  
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -60,6 +83,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.body.delete];
   res.redirect('/urls')
 })
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
