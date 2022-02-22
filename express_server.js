1d530f5601da382fb6aabcdbd25aa1cdbd8304c3
Ryan Williams <r.d.williams.ca@gmail.com>
2022-02-22 04:54:45 +0000

    implemented routing and froont end of edit long url, just need to finish the logic in the server file

--- express_server.js
+++ express_server.js
@@ -38,16 +38,24 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get('/urls/:shortURL', (req, res) => {
+app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+})
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:id", (req, res) => {
+  const newUrl = req.body.newURL;
 })
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.body.delete];
   res.redirect('/urls')
 })
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
